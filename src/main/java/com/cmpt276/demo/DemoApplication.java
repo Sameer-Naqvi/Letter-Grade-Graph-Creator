150b927e115eea17c696cbd88fa276fbb35f83a8
Sameer-Naqvi <sameerabbasnaqvi189@gmail.com>
2023-05-29 22:29:26 -0700

    newest commit

--- src/main/java/com/cmpt276/demo/DemoApplication.java
+++ src/main/java/com/cmpt276/demo/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment1 {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Assignment1.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
